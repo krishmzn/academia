7f70c8f8e0a012bcb495565451c638e08f84d39a
krishmzn11 <kimmhrz@gmail.com>
2022-06-21 12:02:40 +0545

    card shadow

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.getElementById("submit").addEventListener("click", function(){
     window.alert("You've sescessfully registered into It Club Academia");
 })
+
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector("nav")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll("li").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
